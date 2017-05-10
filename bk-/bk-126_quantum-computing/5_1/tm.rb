fa5791743e3a8982875f33475d22e66070949646
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-05-10 18:17:58 +0900

    res 245 bk-126 5#1.2 / 『量子コンピュータとは何か』　中嶋彰 / 20170510_181057
    steps=#1
    ------
    tm.rb => continue
    
            ==> done
    
    ----------- TEMPLATE
    
            ==> done

--- bk-/bk-126_quantum-computing/5_1/tm.rb
+++ bk-/bk-126_quantum-computing/5_1/tm.rb
@@ -16,12 +16,12 @@ at: 2017/05/08 18:20:17
 
 =end
 
-def get_str_current_aux(str_current, loc_head, aryof_index_changed)
+def get_str_current_aux(str_current, loc_Head, aryof_index_changed)
   
   #ref map index http://d.hatena.ne.jp/idesaku/20100403/1270321741
   str_current_aux = str_current.map.with_index {|elem, i|
     
-    if i == loc_head
+    if i == loc_Head
       
 #      puts "[#{File.basename(__FILE__)}:#{__LINE__}] i == head ---> i = #{i.to_s}"
       
@@ -36,28 +36,43 @@ def get_str_current_aux(str_current, loc_head, aryof_index_changed)
   
 end
 
+def aux_add_head_mark(str_Target, loc_Head)
+  
+  return str_Target.map.with_index {|elem, i|
+  #  tmp = str_current.map!.with_index {|elem, i|
+      
+      if i == loc_Head
+        
+        puts "[#{File.basename(__FILE__)}:#{__LINE__}] i == head ---> i = #{i.to_s}"
+        
+        elem = elem + "*"
+        
+      else
+        
+        elem
+        
+      end
+      
+    }
+  
+end#aux_add_head_mark(str_Target, loc_Head)
+
 def exec
 
   ##### command set
   cmd = [
     
-        [
         # command : 1
-          [1, 0, "L"],
-          [0, 0, "L"]
-          ],
+        [[1, 0, "L"],  [0, 0, "L"]],
         # command : 2
-        [
-          [1, 0, "L"],
-          [0, 1, "x"]
-          ]
-    
+        [[1, 0, "L"],  [0, 1, "x"]]
     ]
   
   ##### initial loc of head
-  loc_head_initial = 4
+  loc_Head_initial = 4
     
-  loc_head = loc_head_initial
+  ##### setup : loc of head
+  loc_Head = loc_Head_initial
   
   ##### initial : str_current
   str = "010011101"
@@ -73,23 +88,24 @@ def exec
   
   p aryof_index_changed
     
-  ### str ---> add head mark
-  str_current_aux = str_current.map.with_index {|elem, i|
-#  tmp = str_current.map!.with_index {|elem, i|
-    
-    if i == loc_head
-      
-      puts "[#{File.basename(__FILE__)}:#{__LINE__}] i == head ---> i = #{i.to_s}"
-      
-      elem = elem + "*"
-      
-    else
-      
-      elem
-      
-    end
-    
-  }
+  ##### str ---> add head mark
+  str_current_aux = aux_add_head_mark(str_current, loc_Head)
+#  str_current_aux = str_current.map.with_index {|elem, i|
+##  tmp = str_current.map!.with_index {|elem, i|
+#    
+#    if i == loc_head
+#      
+#      puts "[#{File.basename(__FILE__)}:#{__LINE__}] i == head ---> i = #{i.to_s}"
+#      
+#      elem = elem + "*"
+#      
+#    else
+#      
+#      elem
+#      
+#    end
+#    
+#  }
   
   
 #  str_current.each_with_index {|elem, i|
@@ -123,29 +139,89 @@ def exec
   
   p cmd[0]
 
-  ### report: head location
-  puts "[#{File.basename(__FILE__)}:#{__LINE__}] head is at => #{loc_head}"
+  ##### report: head location
+  puts "[#{File.basename(__FILE__)}:#{__LINE__}] head is at => #{loc_Head}"
+
+  ##### read state value
+  puts "[#{File.basename(__FILE__)}:#{__LINE__}] state value => #{str_current[loc_Head]}"
   
-  ### read state value
-  puts "[#{File.basename(__FILE__)}:#{__LINE__}] state value => #{str_current[loc_head]}"
-  
-  ### read: command
-  command_current = str_current[loc_head].to_i == 1 ? cmd[0][0] : cmd[0][1]
+  ##### read: command
+  command_current = str_current[loc_Head].to_i == 1 ? cmd[0][0] : cmd[0][1]
   
   puts "[#{File.basename(__FILE__)}:#{__LINE__}] command is =>"
   
   p command_current
 
-  ### execute: 1/0
-#  str_current[loc_head] = command_current[1].to_s
-#  str_current[loc_head] = command_current[1].to_s + "+"
+#  ##### execute: 1/0
+  str_current[loc_Head] = command_current[1].to_s
+##  str_current[loc_head] = command_current[1].to_s + "+"
+#  
+#  
   
-  ### report: str_current
+  ##### update : aryof_index_changed
+  if command_current[0] == command_current[1]
+  
+    
+  
+  else#if (command_current[0] == command_current[1])
+  
+    aryof_index_changed[loc_Head] += 1
+  
+  end#if (command_current[0] == command_current[1])
+  
+  puts "[#{File.basename(__FILE__)}:#{__LINE__}] aryof_index_changed is now =>"
+  
+  p aryof_index_changed
+  
+  ##### report: str_current
   puts "[#{File.basename(__FILE__)}:#{__LINE__}] str_current is now =>"
   
-  ### report: str_current_aux
+  ### update : loc_Head
+  puts "[#{File.basename(__FILE__)}:#{__LINE__}] Evaluating the direction command..."
+  
+  if command_current[2] == "L"
+
+    puts "[#{File.basename(__FILE__)}:#{__LINE__}] Moving the head toward => Left"
+      
+    loc_Head -= 1
     
-  p str_current
+    ## validate
+    if loc_Head < 0
+    
+      puts "[#{File.basename(__FILE__)}:#{__LINE__}] loc_Head => less than zero : #{loc_Head.to_s}"
+      
+      exit -1
+    
+    end#if loc_Head > str_current.length - 1
+    
+  elsif command_current[2] == "R"
+
+    puts "[#{File.basename(__FILE__)}:#{__LINE__}] Moving the head toward => Right"
+    
+    loc_Head += 1
+    
+    ## validate
+    if loc_Head > str_current.length - 1
+    
+      puts "[#{File.basename(__FILE__)}:#{__LINE__}] loc_Head => out of limit : #{(str_current.length - 1).to_s}"
+      
+      exit -1
+    
+    end#if loc_Head > str_current.length - 1
+    
+  else#if (command_current[2] == )
+  
+    puts "[#{File.basename(__FILE__)}:#{__LINE__}] Unknown direction command => '#{command_current[2]}'"
+
+    exit -1
+      
+  end#if (command_current[2] == )
+  
+  
+  ##### report: str_current_aux
+  
+  p aux_add_head_mark(str_current, loc_Head)
+#  p str_current
   
   
       
