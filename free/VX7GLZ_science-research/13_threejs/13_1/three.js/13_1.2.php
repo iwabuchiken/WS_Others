0036cbab3ce6b9ec84ac5fa112241610abcee0f6
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-05-10 15:06:04 +0900

    res free# VX7GLZ 13#1.2 / science research / get to know three.js /
    steps=~#2.1.1
    ------
    #) SEG-1
    "Back" link ---> change font size in relation with the user agent
    
            ==> done
    
    ----------- TEMPLATE
    
            ==> done

--- free/VX7GLZ_science-research/13_threejs/13_1/three.js/13_1.2.php
+++ free/VX7GLZ_science-research/13_threejs/13_1/three.js/13_1.2.php
@@ -9,9 +9,20 @@ C:\WORKS_2\WS\WS_Others\free\VX7GLZ_science-research\13_threejs\13_1\
 
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta charset="UTF-8">
-<title>VX7GLZ 13#1.2</title>
+
+	<meta charset="UTF-8">
+	
+	<!-- ref C:\WORKS_2\WS\WS_Others\free\VX7GLZ_science-research\13_threejs\13_1\sample (1)\index.html -->
+	<!-- 	ref http://www.tagindex.com/html5/page/meta_viewport.html -->
+	<meta name="viewport" content="minimum-scale=0.5">
+<!-- 	<meta name="viewport" content="width=180, minimum-scale=0.5"> -->
+<!-- 	<meta name="viewport" content="width=480"> -->
+<!-- 	<meta name="viewport" content="width=device-width"> -->
+	
+	<title>VX7GLZ 13#1.2</title>
+	
 </head>
 
 <?php 
@@ -24,19 +35,67 @@ C:\WORKS_2\WS\WS_Others\free\VX7GLZ_science-research\13_threejs\13_1\
 	//ref http://php.net/manual/ja/reserved.variables.server.php
 	$hostname = $_SERVER['SERVER_NAME'];
 	
+	//ref http://stackoverflow.com/questions/4117555/simplest-way-to-detect-a-mobile-device
+	$useragent=$_SERVER['HTTP_USER_AGENT'];
+	
+	$fname_js_main = "main.js";
+	$proj_root_local = "/WS_Others/free/VX7GLZ_science-research/13_threejs/13_1/three.js";
+	
+	$font_size = "";
+	$font_size_Local = "5";
+	$font_size_Remote = "10";
+	
+// 	echo "hostname = $hostname";
+// 	echo "<br>";
+	
+// 	echo "useragent = $useragent";
+// 		// 	Mozilla/5.0 (Windows NT 6.1; Win64; x64)
+// 	echo "<br>";
+
+	//ref http://php.net/manual/ja/function.preg-match.php
+	//ref http://stackoverflow.com/questions/4117555/simplest-way-to-detect-a-mobile-device answered Nov 7 '10 at 12:01
+	$subject = $useragent;
+	$pattern = '/iPhone/';
+// 	$pattern = '/\(iphone/';
+// 	$pattern = '/(iphone/';
+	$res = preg_match($pattern, $subject, $matches, PREG_OFFSET_CAPTURE, 3);
+	
+// 	echo "subject='$useragent' | pattern='$pattern' | res = $res";
+	
+	
 	if ($hostname == "localhost") {
 	
-		$script_src = "/WS_Others/free/VX7GLZ_science-research/13_threejs/13_1/three.js/js/three.min.js";
-		$script_src_main = "/WS_Others/free/VX7GLZ_science-research/13_threejs/13_1/three.js/js/main.js";
+		$script_src = "$proj_root_local/js/three.min.js";
+// 		$script_src = "/WS_Others/free/VX7GLZ_science-research/13_threejs/13_1/three.js/js/three.min.js";
+// 		$script_src_main = "/WS_Others/free/VX7GLZ_science-research/13_threejs/13_1/three.js/js/$fname_js_main";
+		$script_src_main = "$proj_root_local/js/$fname_js_main";
 		
-		$page_root = "/WS_Others/free/VX7GLZ_science-research/13_threejs/13_1/three.js";
+		$page_root = $proj_root_local;
+// 		$page_root = "/WS_Others/free/VX7GLZ_science-research/13_threejs/13_1/three.js";
+
+		$font_size = $font_size_Local;
 	
 	} else {
 	
 		$script_src = "/WS/WS_Others/free/VX7GLZ_science-research/13_threejs/13_1/three.js/js/three.min.js";
-		$script_src_main = "/WS/WS_Others/free/VX7GLZ_science-research/13_threejs/13_1/three.js/js/main.js";
+		$script_src_main = "/WS/WS_Others/free/VX7GLZ_science-research/13_threejs/13_1/three.js/js/$fname_js_main";
+// 		$script_src_main = "/WS/WS_Others/free/VX7GLZ_science-research/13_threejs/13_1/three.js/js/main.js";
 	
 		$page_root = "/WS/WS_Others/free/VX7GLZ_science-research/13_threejs/13_1/three.js";
+		
+		// set font size ---> use : user agent
+		if ($res == 1) {
+		
+			$font_size = $font_size_Remote;
+		
+		} else {
+		
+			$font_size = $font_size_Local;
+			
+		}//if ($res == 1)
+		
+		
+// 		$font_size = $font_size_Remote;
 		
 	}
 
@@ -44,7 +103,18 @@ C:\WORKS_2\WS\WS_Others\free\VX7GLZ_science-research\13_threejs\13_1\
 
 <body>
 
-	<a href="<?php echo $page_root?>">Back</a>
+	<a href="
+		
+			<?php echo $page_root?>
+		
+		">
+<!-- 		ref https://www.w3schools.com/tags/tag_font.asp -->
+			<font size="<?php echo $font_size; ?>">Back</font>
+	</a>
+	
+<!-- 	<a href=" -->
+		<?php //echo $page_root?>
+<!-- 		">Back</a> -->
 	
 	<hr>
 
--- free/VX7GLZ_science-research/13_threejs/13_1/three.js/index.php
+++ free/VX7GLZ_science-research/13_threejs/13_1/three.js/index.php
@@ -1,3 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<meta charset="UTF-8">
+	
+	<!-- ref C:\WORKS_2\WS\WS_Others\free\VX7GLZ_science-research\13_threejs\13_1\sample (1)\index.html -->
+<!-- 	<meta name="viewport" content="width=device-width"> -->
+	
+	<meta name="viewport" content="minimum-scale=0.5">
+	
+	<title>index : threejs project</title>
+	
+
+</head>
+<body>
+
+<?php 
+
+	//ref http://php.net/manual/ja/reserved.variables.server.php
+	$hostname = $_SERVER['SERVER_NAME'];
+	
+	//ref http://stackoverflow.com/questions/4117555/simplest-way-to-detect-a-mobile-device
+	$useragent=$_SERVER['HTTP_USER_AGENT'];
+
+	$font_size = "";
+	$font_size_Local = "4";
+	$font_size_Remote = "10";
+	
+	$subject = $useragent;
+	$pattern = '/iPhone/';
+	// 	$pattern = '/\(iphone/';
+	// 	$pattern = '/(iphone/';
+	$res = preg_match($pattern, $subject, $matches, PREG_OFFSET_CAPTURE, 3);
+
+	// set font size ---> use : user agent
+	if ($res == 1) {
+	
+		$font_size = $font_size_Remote;
+	
+	} else {
+	
+		$font_size = $font_size_Local;
+			
+	}//if ($res == 1)
+	
+
+?>
+
 <?php
 	//http://phpspot.net/php/pg%E3%83%87%E3%82%A3%E3%83%AC%E3%82%AF%E3%83%88%E3%83%AA%E3%81%AE%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E3%83%AA%E3%82%B9%E3%83%88%E3%82%92%E5%8F%96%E5%BE%97.html
 	
@@ -40,8 +90,14 @@
 		
 		foreach ($file_names as $name) {
 			
-			echo $count.". "."<a href='".$name."'>"."$name"."</a>"."\n";
+			echo $count.". "."<a href='".$name."'>"
+				
+				."<font size=\"$font_size\">"
+				."$name"
+				."</font>"
+				."</a>"."\n";
 			echo "<br/>";
+// 			echo "<br/>";
 			
 			$count += 1;
 		}
@@ -53,3 +109,5 @@
 	//echo `whoami`;
 	
 ?>
+
+</body>
