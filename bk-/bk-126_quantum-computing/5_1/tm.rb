cc28e73588cb20d75ad4e78b78566f68d96156e0
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-05-15 12:32:23 +0900

    res free# VX7GLZ 19#1 / science research / turing machine / 20170515_110838
    steps=~#4
    ------
    executions ---> do with for loop : exec_V3
    #) commands ---> 2 command sets
    For loop ---> rest of the command sets from the book
    video : https://www.youtube.com/watch?v=gJQTFhkhwPA
    0:39 ==> daigram
    #) command --->generate randomly
    
            ==> done
    
    ----------- TEMPLATE
    
            ==> done

--- bk-/bk-126_quantum-computing/5_1/tm.rb
+++ bk-/bk-126_quantum-computing/5_1/tm.rb
@@ -46,7 +46,7 @@ def aux_add_head_mark(str_Target, loc_Head)
       
       if i == loc_Head
         
-        puts "[#{File.basename(__FILE__)}:#{__LINE__}] i == head ---> i = #{i.to_s}"
+#        puts "[#{File.basename(__FILE__)}:#{__LINE__}] i == head ---> i = #{i.to_s}"
         
         elem = elem + "*"
         
@@ -69,17 +69,17 @@ def aux_add_changed(str_Target, aryof_index_changed)
       if aryof_index_changed[i] > 0
 #      if i == loc_Head
         
-        puts "[#{File.basename(__FILE__)}:#{__LINE__}] "\
-            "aryof_index_changed[i] > 0 ==> #{aryof_index_changed[i].to_s}"
-        
-        puts "[#{File.basename(__FILE__)}:#{__LINE__}] elem => '#{elem}'"
+#        puts "[#{File.basename(__FILE__)}:#{__LINE__}] "\
+#            "aryof_index_changed[i] > 0 ==> #{aryof_index_changed[i].to_s}"
+#        
+#        puts "[#{File.basename(__FILE__)}:#{__LINE__}] elem => '#{elem}'"
         
 #        elem + "+"
         
 #        puts "[#{File.basename(__FILE__)}:#{__LINE__}] i == head ---> i = #{i.to_s}"
         aryof_index_changed[i].times do
           
-          puts "[#{File.basename(__FILE__)}:#{__LINE__}] update elem..."
+#          puts "[#{File.basename(__FILE__)}:#{__LINE__}] update elem..."
           
           elem = elem + "+"
 #          elem + "+"
@@ -104,11 +104,11 @@ def aux_add_changed(str_Target, aryof_index_changed)
   }
   
   ##### report
-  print "[#{File.basename(__FILE__)}:#{__LINE__}] target => "
-  p str_Target
-  
-  print "[#{File.basename(__FILE__)}:#{__LINE__}] index changed => "
-  p tmp
+#  print "[#{File.basename(__FILE__)}:#{__LINE__}] target => "
+#  p str_Target
+#  
+#  print "[#{File.basename(__FILE__)}:#{__LINE__}] index changed => "
+#  p tmp
   
   return tmp
   
@@ -130,48 +130,48 @@ end#add_aux(str_Target, aryOf_Index_Changed, loc_Head)
 
 def exec_1_step(str_Target, aryof_index_changed, loc_Head, cmd)
 
-  print "[#{File.basename(__FILE__)}:#{__LINE__}] cmd => "
-  p cmd
+#  print "[#{File.basename(__FILE__)}:#{__LINE__}] cmd => "
+#  p cmd
   
   str_current_aux = aux_add_head_mark(str_Target, loc_Head)
   
-  puts "[#{File.basename(__FILE__)}:#{__LINE__}] str_Target =>"
-  
-  p str_Target
-  
-  puts "[#{File.basename(__FILE__)}:#{__LINE__}] str_current_aux =>"
-  
-  p str_current_aux
+#  puts "[#{File.basename(__FILE__)}:#{__LINE__}] str_Target =>"
+#  
+#  p str_Target
+#  
+#  puts "[#{File.basename(__FILE__)}:#{__LINE__}] str_current_aux =>"
+#  
+#  p str_current_aux
   
   puts
   
   #########################
   ##### 
   #####     q0 --> q1
-  ##### 
+  #### 
   #########################
-  puts "========== q0 ---> q1 =========="
+#  puts "========== q0 ---> q1 =========="
   
-  print "[#{File.basename(__FILE__)}:#{__LINE__}] command 1st (cmd[0]) => "
+#  print "[#{File.basename(__FILE__)}:#{__LINE__}] command 1st (cmd[0]) => "
 #  puts "[#{File.basename(__FILE__)}:#{__LINE__}] command 1st =>"
   
-  p cmd    #=> 
+#  p cmd    #=> 
 #  p cmd[0]    #=> '[1, 0, "L"]'
 
-  ##### report: head location
-  puts "[#{File.basename(__FILE__)}:#{__LINE__}] head is at => #{loc_Head}"
-
-  ##### read state value
-  puts "[#{File.basename(__FILE__)}:#{__LINE__}] state value => #{str_Target[loc_Head]}"
+#  ##### report: head location
+#  puts "[#{File.basename(__FILE__)}:#{__LINE__}] head is at => #{loc_Head}"
+#
+#  ##### read state value
+#  puts "[#{File.basename(__FILE__)}:#{__LINE__}] state value => #{str_Target[loc_Head]}"
   
   ##### read: command
   command_current = str_Target[loc_Head].to_i == 1 ? cmd[0] : cmd[1]
 #  command_current = str_Target[loc_Head].to_i == 1 ? cmd[0][0] : cmd[0][1]
   
-  print "[#{File.basename(__FILE__)}:#{__LINE__}] command is => "
+#  print "[#{File.basename(__FILE__)}:#{__LINE__}] command is => "
 #  puts "[#{File.basename(__FILE__)}:#{__LINE__}] command is =>"
   
-  p command_current
+#  p command_current
 
 #  ##### execute: 1/0
   str_Target[loc_Head] = command_current[1].to_s
@@ -179,8 +179,8 @@ def exec_1_step(str_Target, aryof_index_changed, loc_Head, cmd)
 #  
 #  
   
-  print "[#{File.basename(__FILE__)}:#{__LINE__}] str_Target changed => "
-  p str_Target
+#  print "[#{File.basename(__FILE__)}:#{__LINE__}] str_Target changed => "
+#  p str_Target
   
   ##### update : aryof_index_changed
   if command_current[0] == command_current[1]
@@ -193,9 +193,9 @@ def exec_1_step(str_Target, aryof_index_changed, loc_Head, cmd)
   
   end#if (command_current[0] == command_current[1])
   
-  print "[#{File.basename(__FILE__)}:#{__LINE__}] aryof_index_changed is now => "
-  
-  p aryof_index_changed
+#  print "[#{File.basename(__FILE__)}:#{__LINE__}] aryof_index_changed is now => "
+#  
+#  p aryof_index_changed
   
 #  ##### update : 
 #  aux_add_head_mark(str_current, aryof_index_changed)
@@ -205,20 +205,20 @@ def exec_1_step(str_Target, aryof_index_changed, loc_Head, cmd)
 #  puts "[#{File.basename(__FILE__)}:#{__LINE__}] str_current is now =>"
   
   ### update : loc_Head
-  puts
-  puts "[#{File.basename(__FILE__)}:#{__LINE__}] Evaluating the direction command"
+#  puts
+#  puts "[#{File.basename(__FILE__)}:#{__LINE__}] Evaluating the direction command"
 #  puts "[#{File.basename(__FILE__)}:#{__LINE__}] Evaluating the direction command..."
   
-  print "[#{File.basename(__FILE__)}:#{__LINE__}] command_current => "
-  p command_current     #=> '[1, 0, "L"]'
+#  print "[#{File.basename(__FILE__)}:#{__LINE__}] command_current => "
+#  p command_current     #=> '[1, 0, "L"]'
   
   if command_current[2] == "L"
 
-    print "[#{File.basename(__FILE__)}:#{__LINE__}] command is => "
-    
-    puts " => 'L'"
-    
-    puts "[#{File.basename(__FILE__)}:#{__LINE__}] Moving the head toward => Left"
+#    print "[#{File.basename(__FILE__)}:#{__LINE__}] command is => "
+#    
+#    puts " => 'L'"
+#    
+#    puts "[#{File.basename(__FILE__)}:#{__LINE__}] Moving the head toward => Left"
       
     loc_Head -= 1
     
@@ -233,9 +233,9 @@ def exec_1_step(str_Target, aryof_index_changed, loc_Head, cmd)
     
   elsif command_current[2] == "R"
 
-    puts " => 'R'"
-    
-    puts "[#{File.basename(__FILE__)}:#{__LINE__}] Moving the head toward => Right"
+#    puts " => 'R'"
+#    
+#    puts "[#{File.basename(__FILE__)}:#{__LINE__}] Moving the head toward => Right"
     
     loc_Head += 1
     
@@ -250,11 +250,11 @@ def exec_1_step(str_Target, aryof_index_changed, loc_Head, cmd)
     
   elsif command_current[2] == "x"
 
-    print "[#{File.basename(__FILE__)}:#{__LINE__}] command is => "
-    
-    puts " => 'x'"
-    
-    puts "[#{File.basename(__FILE__)}:#{__LINE__}] Moving the head toward => No movement"
+#    print "[#{File.basename(__FILE__)}:#{__LINE__}] command is => "
+#    
+#    puts " => 'x'"
+#    
+#    puts "[#{File.basename(__FILE__)}:#{__LINE__}] Moving the head toward => No movement"
     
 #    loc_Head += 1
     
@@ -283,7 +283,7 @@ def exec_1_step(str_Target, aryof_index_changed, loc_Head, cmd)
 #  p str_current_aux
   
       
-  puts "[#{File.basename(__FILE__)}:#{__LINE__}] done"
+#  puts "[#{File.basename(__FILE__)}:#{__LINE__}] done"
   
   ################################
   #	
@@ -294,7 +294,176 @@ def exec_1_step(str_Target, aryof_index_changed, loc_Head, cmd)
 
   
 end#exec_1_step(str_Target, aryof_index_changed, loc_Head)
+
+
+def get_cmd_array(numOf_cmds = 3)
+
+    cmd = [
+    
+        #ref https://www.youtube.com/watch?v=gJQTFhkhwPA 0:39
+        # command : 1
+        [[1, 0, "L"],  [0, 0, "L"]],  #=> q0 ---> q1
+        # command : 2
+        [[1, 0, "L"],  [0, 1, "x"]],  #=> q1 ---> q2
+          
+        [[1, 1, "R"],  [0, 0, "L"]],  #=> q2 ---> q0
+          
+        [[1, 0, "x"],  [0, 0, "x"]]  #=> q0 ---> qH
+    ]
+
+    # more commands
+    if numOf_cmds > 3
+    
+      (numOf_cmds - 3).times do
+        
+        cmd << [[1, rand(2), rand(2) == 1 ? "L" : "R"], [0, rand(2), rand(2) == 1 ? "L" : "R"]]
+          
+      end
+    
+    end#if numOf_cmds > 3
+    
+    #ref http://stackoverflow.com/questions/198460/how-to-get-a-random-number-in-ruby
+#    cmd << [[1, rand(2), rand(2) == 1 ? "L" : "R"], [0, rand(2), rand(2) == 1 ? "L" : "R"]]
+#    cmd << [[1, rand(2), "L"], [0, rand(2), "L"]]
+#    cmd << [[1, 0, "L"], [0, 1, "L"]]
+    
+      
+      
+    return cmd
+      
+end#get_cmd_array(numOf_cmds = 3)
+
+def exec_V3
+
+  ##### command set
+  cmd = get_cmd_array(5)
+#  cmd = get_cmd_array(3)
+
+  #  cmd = [
+#    
+#        #ref https://www.youtube.com/watch?v=gJQTFhkhwPA 0:39
+#        # command : 1
+#        [[1, 0, "L"],  [0, 0, "L"]],  #=> q0 ---> q1
+#        # command : 2
+#        [[1, 0, "L"],  [0, 1, "x"]],  #=> q1 ---> q2
+#          
+#        [[1, 1, "R"],  [0, 0, "L"]],  #=> q2 ---> q0
+#          
+#        [[1, 0, "x"],  [0, 0, "x"]]  #=> q0 ---> qH
+#    ]
   
+  ##### initial loc of head
+  loc_Head_initial = 4
+    
+  ##### setup : loc of head
+  loc_Head = loc_Head_initial
+  
+  ##### initial : str_Current
+  str = "010011101"
+  
+  puts "[#{File.basename(__FILE__)}:#{__LINE__}] str => '#{str}'"
+  
+  
+  str_Current = str.split("");
+  
+  print "[#{File.basename(__FILE__)}:#{__LINE__}] str_Current => "
+  p str_Current
+
+  ##### initial : aryOf_Index_Changed
+  aryOf_Index_Changed = Array.new(str_Current.length, 0)
+  
+  print "[#{File.basename(__FILE__)}:#{__LINE__}] aryOf_Index_Changed created => "
+  
+  p aryOf_Index_Changed
+
+  ##### history
+  aryOf_Histories = Array.new()
+  
+  tmp = add_aux(str_Current, aryOf_Index_Changed, loc_Head)
+  
+  print "[#{File.basename(__FILE__)}:#{__LINE__}] str_Current => "
+  p str_Current
+  
+  # 'orig' is needed ===> str_Current gets chagned at each iteration; for history
+  #   purpose, use orig
+  orig = Array.new()
+  
+  str_Current.each {|elem| orig << elem  }
+
+  
+  aryOf_Histories << [tmp, orig]
+#  aryOf_Histories << [tmp, str_Current]
+#  aryOf_Histories << [add_aux(str_Current, aryOf_Index_Changed, loc_Head), str_Current]
+#  aryOf_Histories << add_aux(str_Current, aryOf_Index_Changed, loc_Head)
+  
+  print "[#{File.basename(__FILE__)}:#{__LINE__}] aryOf_Histories => "
+  p aryOf_Histories
+  
+  ################################
+  #	
+  #	operations
+  #
+  ################################
+  cmd.each_with_index {|elem, i|
+    
+#    p elem
+    
+    ##### operations
+    str_Current, aryOf_Index_Changed, loc_Head = \
+      exec_1_step(str_Current, aryOf_Index_Changed, loc_Head, elem)
+
+    ##### report: str_current_aux
+    str_current_aux = add_aux(str_Current, aryOf_Index_Changed, loc_Head)
+#    str_current_aux = add_aux(str_Current, aryOf_Index_Changed, loc_Head)
+    
+#    print "[#{File.basename(__FILE__)}:#{__LINE__}] index ---> processed: "
+#    p str_current_aux
+      
+    ##### add : history
+    orig = Array.new()
+    
+    str_Current.each {|elem|
+     
+      orig << elem
+       
+    }
+    
+    print "[#{File.basename(__FILE__)}:#{__LINE__}] orig => "
+    p orig
+    
+    aryOf_Histories << [str_current_aux.map{|x|x}, orig]
+#    aryOf_Histories << [str_current_aux.map{|x|x}, str_Current.map{|x|x}]
+#    aryOf_Histories << [str_current_aux, str_Current]
+#    aryOf_Histories << str_current_aux
+    
+    puts "[#{File.basename(__FILE__)}:#{__LINE__}] iteration #{i.to_s} => "
+    aryOf_Histories.each {|elem|
+     
+      p elem
+       
+    }
+    
+  }#cmd.each_with_index {|elem, i|
+
+  puts "[#{File.basename(__FILE__)}:#{__LINE__}] histories =>"
+  
+  aryOf_Histories.each_with_index {|elem, i|
+  
+    puts "----- history : #{i.to_s} -----"
+#    p elem
+#    puts elem
+    #ref to_i(2) http://stackoverflow.com/questions/2339695/how-to-convert-a-string-or-integer-to-binary-in-ruby answered Feb 26 '10 at 8:52
+    p elem, elem[1], elem[1].join(""), elem[1].join("").to_i(2)
+#    p elem, elem[1], elem[1].join("")
+#    p elem, elem[1], elem[1].join("")
+#    p elem, elem[1].join("").to_i(2)
+  
+  }
+  
+  return
+      
+end#exec_V3
+
 def exec_V2
 
   ##### command set
@@ -615,5 +784,6 @@ def exec
 end
 
 #exec
-exec_V2
+#exec_V2
+exec_V3
 
