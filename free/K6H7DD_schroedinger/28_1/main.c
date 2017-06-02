b6426f27539fcea66b74263c0ff9e68ad1adfa3b
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-06-02 21:17:53 +0900

    res free# / K6H7DD 28#2 / シュレーディンガー方程式 / 20170602_180556
    steps=~#1.3.3
    ------
    ==> see mail
    
            ==> done
    
    ----------- TEMPLATE
    
            ==> done

--- free/K6H7DD_schroedinger/28_1/main.c
+++ free/K6H7DD_schroedinger/28_1/main.c
@@ -40,6 +40,14 @@ void generate_Solutions() {
 	psys = malloc(sizeof(double) * (max / tick));
 	fs = malloc(sizeof(double) * (max / tick));
 
+	double psy_prev = psy_0;
+	double f_prev = f_0;
+
+	double psy_cur;
+	double f_cur;
+
+	double h2_2m = 38.14;
+
 	psys[0] = psy_0;
 	fs[0] = f_0;
 
@@ -50,21 +58,26 @@ void generate_Solutions() {
 	// loop
 	//ref https://stackoverflow.com/questions/18175513/how-to-increment-a-for-loop-with-a-decimal-value-in-c
 	for (i = 1; i <= max; i ++) {
-//	for (i = 1; i < max; i ++) {
-//	for (i = 0.1; i < max; i += tick) {
-//	for (i = 0.1; i < max; i = i + tick) {
-
-//		//debug
-//		printf("[%s:%d] i = %d\n", basename(__FILE__, '\\'), __LINE__, i);
-////		printf("[%s:%d] i = %f\n", basename(__FILE__, '\\'), __LINE__, i);
-
 
 		// V : potential energy
 		if (i * tick < 5 || i * tick >= 15) V = 50.0;
-//		if (i < 5 || i >= 15) V = 50.0;
 		else V = 0.0;
 
+		// calc
+		psy_cur = tick * f_prev + psy_prev;
 
+		f_cur = -1 * tick * psy_prev * (E - V) / h2_2m + f_prev;
+
+		psys[i]	= psy_cur;
+		fs[i]	= f_cur;
+
+		printf("[%s:%d] psy_cur = %2.7f / f_cur = %2.7f\n",
+				basename(__FILE__, '\\'), __LINE__, psy_cur, f_cur);
+
+
+		// update prev
+		psy_prev	= psy_cur;
+		f_prev		= f_cur;
 
 	}//for (i = 0; i < 20; ++i) {
 
@@ -73,6 +86,168 @@ void generate_Solutions() {
 
 	// free
 	free(psys);
+	free(fs);
+
+	//debug
+	printf("[%s:%d] generate_Solutions() ---> done\n", basename(__FILE__, '\\'), __LINE__);
+
+
+}//void generate_Solutions()
+
+void generate_Solutions_2() {
+
+	double psy_0 = 0.0;
+	double f_0 = 0.1;
+
+	double V;	// potential energy
+
+	double E = 2.6760079;	// Energy of the electron
+
+	double tick = 0.1;
+//	float tick = 0.1;
+
+	int i;		// iterator
+
+	int width = 20;
+
+	int max = width / tick;	// x coordinate where psy gets to be zero
+//	double max = 20;	// x coordinate where psy gets to be zero
+//	int max = 20;	// x coordinate where psy gets to be zero
+
+	double* psys;
+	double* fs;
+
+	psys = malloc(sizeof(double) * (max / tick));
+	fs = malloc(sizeof(double) * (max / tick));
+
+	double psy_prev = psy_0;
+	double f_prev = f_0;
+
+	double psy_cur;
+	double f_cur;
+
+	double h2_2m = 38.14;
+
+	psys[0] = psy_0;
+	fs[0] = f_0;
+
+	printf("[%s:%d] psys[0] => %2.7f\n", basename(__FILE__, '\\'), __LINE__, psys[0]);
+	printf("[%s:%d] fs[0] => %2.7f\n", basename(__FILE__, '\\'), __LINE__, fs[0]);
+
+
+	// loop
+	//ref https://stackoverflow.com/questions/18175513/how-to-increment-a-for-loop-with-a-decimal-value-in-c
+	for (i = 1; i <= max; i ++) {
+
+		// V : potential energy
+		if (i * tick < 5 || i * tick >= 15) V = 50.0;
+		else V = 0.0;
+
+		// calc
+		psy_cur = tick * f_prev + psy_prev;
+
+		f_cur = -1 * tick * psy_prev * (E - V) / h2_2m + f_prev;
+
+		psys[i]	= psy_cur;
+		fs[i]	= f_cur;
+
+		printf("[%s:%d] psy_cur = %2.7f / f_cur = %2.7f\n",
+				basename(__FILE__, '\\'), __LINE__, psy_cur, f_cur);
+
+
+		// update prev
+		psy_prev	= psy_cur;
+		f_prev		= f_cur;
+
+	}//for (i = 0; i < 20; ++i) {
+
+
+
+
+	// free
+	free(psys);
+	free(fs);
+
+	//debug
+	printf("[%s:%d] generate_Solutions() ---> done\n", basename(__FILE__, '\\'), __LINE__);
+
+
+}//void generate_Solutions()
+
+void exec_Generate_Solutions
+(double E, double tick, double* psys, double* fs) {
+
+	double psy_0 = 0.0;
+	double f_0 = 0.1;
+
+	double V;	// potential energy
+
+	double E = 2.6760079;	// Energy of the electron
+
+	double tick = 0.1;
+//	float tick = 0.1;
+
+	int i;		// iterator
+
+	int width = 20;
+
+	int max = width / tick;	// x coordinate where psy gets to be zero
+//	double max = 20;	// x coordinate where psy gets to be zero
+//	int max = 20;	// x coordinate where psy gets to be zero
+
+//	double* psys;
+//	double* fs;
+
+	psys = malloc(sizeof(double) * (max / tick));
+	fs = malloc(sizeof(double) * (max / tick));
+
+	double psy_prev = psy_0;
+	double f_prev = f_0;
+
+	double psy_cur;
+	double f_cur;
+
+	double h2_2m = 38.14;
+
+	psys[0] = psy_0;
+	fs[0] = f_0;
+
+	printf("[%s:%d] psys[0] => %2.7f\n", basename(__FILE__, '\\'), __LINE__, psys[0]);
+	printf("[%s:%d] fs[0] => %2.7f\n", basename(__FILE__, '\\'), __LINE__, fs[0]);
+
+
+	// loop
+	//ref https://stackoverflow.com/questions/18175513/how-to-increment-a-for-loop-with-a-decimal-value-in-c
+	for (i = 1; i <= max; i ++) {
+
+		// V : potential energy
+		if (i * tick < 5 || i * tick >= 15) V = 50.0;
+		else V = 0.0;
+
+		// calc
+		psy_cur = tick * f_prev + psy_prev;
+
+		f_cur = -1 * tick * psy_prev * (E - V) / h2_2m + f_prev;
+
+		psys[i]	= psy_cur;
+		fs[i]	= f_cur;
+
+		printf("[%s:%d] psy_cur = %2.7f / f_cur = %2.7f\n",
+				basename(__FILE__, '\\'), __LINE__, psy_cur, f_cur);
+
+
+		// update prev
+		psy_prev	= psy_cur;
+		f_prev		= f_cur;
+
+	}//for (i = 0; i < 20; ++i) {
+
+
+
+
+//	// free
+//	free(psys);
+//	free(fs);
 
 	//debug
 	printf("[%s:%d] generate_Solutions() ---> done\n", basename(__FILE__, '\\'), __LINE__);
