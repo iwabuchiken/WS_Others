e4fcdd715677a017fb117fb2a07cdc045d474012
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-06-21 14:20:41 +0900

    res free# UH8G6E 2#1 / computer engineering / HTML + canvas / 20170621_123139
    steps=~1.2
    ------
    animation ---> DROP : can't use jquery
    
            ==> DROP
    
    ----------- TEMPLATE
    
            ==> done

--- free/UH8G6E_CE/2_1/js/main.js
+++ free/UH8G6E_CE/2_1/js/main.js
@@ -1,93 +1,62 @@
-function alert_message() {
-	
-	alert("message");
-	
-}
-
-
-
 $(document).ready(function(){
 
-//	alert("ready");
+	alert("ready");
+
+	loop();
 	
 })
 
-function draw_Circle() {
 
-//	alert("drawing a circle...");
+var canvas = document.querySelector('#canvas-container');
+var ctx = canvas.getContext('2d');
+
+var speed = 2;    //移動速度
+var x = 0;      //X軸の位置
+
+// ループ処理
+function loop() {
 	
-    if (!window.HTMLCanvasElement) {
-    	
-    	alert("canvas ---> not supported");
-    	
-    	return;
-    }
-    var canvas = document.querySelector('#canvas-container');
-    var ctx = canvas.getContext('2d');
- 
-    // 円の描画
-    ctx.beginPath();
+	alert("looping...");
+	
+    requestAnimFrame(loop);
     
-    //ref http://www.html5.jp/canvas/ref/method/arc.html
-    // arc(x, y, radius, startAngle, endAngle, anticlockwise)
-//    ctx.arc(100, 100, 20, 0, 2*Math.PI, false);
+    alert("requestAnimFrame => called");
     
-    var x = Math.floor( Math.random() * 200 ) + 100;
-    var y = Math.floor( Math.random() * 200 ) + 100;
+    ctx.clearRect(0,0, ctx.canvas.width, ctx.canvas.height);
+    // ループ毎にxを加算
+    x += speed;
+    // 丸を描画
+    drawCircle(x, 100, 20, '#0000FF');
     
-    var angle_Start = Math.random() * Math.PI * 2;
-    var angle_End = Math.random() * Math.PI * 2;
-    
-    var tmp;
-    
-    if (angle_Start > angle_End) {
-    	
-    	tmp = angle_Start;
-    	
-    	angle_Start = angle_End;
-    	
-    	angle_End = tmp;
-    	
-    }
-    
-    alert("angle_Start => " + angle_Start
-    		+ " / "
-    		+ "angle_End => " + angle_End
-	);
-    
-    ctx.arc(x, y, 20, angle_Start, angle_End, false);
-//    ctx.arc(x, y, 20, Math.PI, 2*Math.PI, false);
-    
-    ctx.fillStyle = '#D0A869';
-    
-//    alert("filling...");
-    
-    ctx.fill();
-
+    if (x > 600) x = 0;
 }
 
-window.onload = function() {
-    // Canvas未サポートは実行しない
-    if (!window.HTMLCanvasElement) {
-    	
-    	alert("canvas ---> not supported");
-    	
-    	return;
-    }
-    var canvas = document.querySelector('#canvas-container');
-    var ctx = canvas.getContext('2d');
- 
-    // 円の描画
+
+function drawCircle(x, y, scale, color) {
     ctx.beginPath();
-    
-    //ref http://www.html5.jp/canvas/ref/method/arc.html
-    // arc(x, y, radius, startAngle, endAngle, anticlockwise)
-    ctx.arc(100, 100, 20, 0, 2*Math.PI, false);
-//    ctx.arc(100, 100, 20, Math.PI, 2*Math.PI, false);
-    
-    ctx.fillStyle = '#D0A869';
-    
-//    alert("filling...");
-    
+    ctx.arc(x, y, scale, 0, 2*Math.PI, false);
+    ctx.fillStyle = color;
     ctx.fill();
-};
+}
+
+window.requestAnimFrame = (function(){
+	
+	alert("requestAnimFrame => starting...");
+	
+    return  window.requestAnimationFrame   ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function(callback){
+            window.setTimeout(callback, 120 / 60);
+        };
+})();
+
+//loop();
+
+//$(document).ready(function(){
+//
+//	alert("ready");
+//	
+//})
