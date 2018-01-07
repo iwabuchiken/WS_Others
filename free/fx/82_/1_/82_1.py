1ac404cd492b9da7ec214cfad8c2b06b58231212
iwabuchiken <iwabuchi.k.2010@gmail.com>
2018-01-07 18:10:00 +0900

    FX / 22-1 s-62*82#2 / fx programming / 20180107_
    ------
    Read csv data
    
            ==> done

--- free/fx/82_/1_/82_1.py
+++ free/fx/82_/1_/82_1.py
@@ -18,12 +18,13 @@ sys.path.append('C:/WORKS_2/WS/WS_Others/free/fx/82_')
 
 # import libfx
 # import libs
-from libs.libfx import *
 # from libs.libfx import *
-# from libs import libs
+# from libs.libfx import *
+from libs import libs
+# from libs.libs import *
 # from libs.libs import *
 # from libs import *
-# from libs import libfx
+from libs import libfx
 
 
 import getopt
@@ -32,40 +33,38 @@ import inspect
 
 import math as math
 
-import wave
+# import wave
 import struct
-import numpy as np
-from pylab import *
+# import numpy as np
+# from pylab import *
 
-from matplotlib import pylab as plt
+# from matplotlib import pylab as plt
 
 import random as rnd
 
 ###############################################
-def linenum(depth=0):
-#     print "line"
-     
-    frame = inspect.currentframe(depth+1)
-    return frame.f_lineno
- 
-def thisfile(depth=0):
-# def _file(depth=0):
-#     print "line"
-     
-    frame = inspect.currentframe(depth+1)
-     
-    return os.path.basename(frame.f_code.co_filename)
-#     return frame.f_code.co_filename
 
 def exec_prog():
     
-#     print "[%s:%d] exec_prog => done" % (__file__, linenum())
+    '''###################
+        get data        
+    ###################'''
+    fname_In = "../data/49_11_file-io.USDJPY.Period-H1.Days-720.Bars-17280.20171231_233725.csv"    #=>
 
-    msg = "[%s:%d] exec_prog => done" % (thisfile(), linenum())
-#     print "[%s:%d] exec_prog => done" % (thisfile(), linenum())
-#     print "[%s:%d] exec_prog => done" % (thisfile(), linenum())
-#     print "[%s:%d] exec_prog => done" % (libs.libs.thisfile(), libs.libs.linenum())
-#     print "[%s:%d] exec_prog => done" % (libs.thisfile(), libs.linenum())
+    id_Start    = 195
+    id_End      = 202
+
+    header_Length = 3
+    skip_Header=True
+
+    result = libfx.get_ChartData_CSV_Between(\
+                    fname_In, id_Start, id_End, header_Length, skip_Header)
+#     result = libfx.get_ChartData_CSV_Between(fname_In, id_Start, id_End)
+    
+    print "[%s:%d] result => %s" % (libs.thisfile(), libs.linenum(), result)
+    
+    
+    print "[%s:%d] exec_prog => done" % (libs.thisfile(), libs.linenum())
 
 #def exec_prog()
 
@@ -87,16 +86,8 @@ if __name__ == "__main__" :
     ###################'''
     exec_prog()
 
-    #test
-#     libs.libfx.test_func() # libfx.py
-#     libfx.test_func() # libfx.py
-    test_func() # libfx.py
-
-    #test
-#     print(os.environ['PYTHONPATH'].split(os.pathsep))
-    print(sys.path)
-    
     print
 #     msg = "[%s:%d] done" % (___FILE, ___FILE)
-    msg = "[%s:%d] done" % (thisfile(), linenum())
+#     msg = "[%s:%d] done" % (thisfile(), linenum())
+    msg = "[%s:%d] get_ChartData_Between(fname, id_Start, id_End) => done" % (libs.thisfile(), libs.linenum())
     print (msg)
--- free/fx/82_/libs/libfx.py
+++ free/fx/82_/libs/libfx.py
@@ -11,24 +11,159 @@ import os
 from time import gmtime, strftime, localtime, time
 from __builtin__ import str
 from sympy.physics.vector.printing import params
+# from curses.ascii import NUL
 
-def linenum(depth=0):
-#     print "line"
-     
-    frame = inspect.currentframe(depth+1)
-    return frame.f_lineno
- 
-def thisfile(depth=0):
-# def _file(depth=0):
-#     print "line"
-     
-    frame = inspect.currentframe(depth+1)
-     
-    return os.path.basename(frame.f_code.co_filename)
-#     return frame.f_code.co_filename
+'''###################
+    libs : C:\WORKS_2\WS\WS_Others\free\fx\82_\libs\libs.py
+###################'''
+import libs
 
+import csv
+import sys
+
+###############################################
 
 def test_func():
     
     print "[%s:%d] test_func()" % (thisfile(), linenum())
     
+
+# def get_ChartData_CSV_Between(fname_In, id_Start, id_End):
+def get_ChartData_CSV_Between \
+(fname_In, id_Start, id_End, header_Length, skip_Header=True):
+    
+    '''###################
+        file : open
+    ###################'''
+#     fname_In = "data/49_11_file-io.USDJPY.Period-H1.Days-720.Bars-17280.20171231_233725.csv"    #=> No such file or directory
+#     fname_In = "../data/49_11_file-io.USDJPY.Period-H1.Days-720.Bars-17280.20171231_233725.csv"    #=> 
+    
+    #ref csv open http://www.pythonforbeginners.com/systems-programming/using-the-csv-module-in-python/
+    f = open(fname_In, 'rb')
+#     fin = open(fname_In, 'r')
+    
+    print "[%s:%d] file => opened : %s" % (libs.thisfile(), libs.linenum(), fname_In)
+    
+    '''###################
+        file : read
+    ###################'''
+    '''###################
+        skip header
+    ###################'''
+    print "[%s:%d] skip headers => %d lines" % \
+                    (libs.thisfile(), libs.linenum(), header_Length)
+    
+    #ref delimiter https://docs.python.org/3.5/library/csv.html
+    delim = ';'
+    reader = csv.reader(f, delimiter=delim)
+#     reader = csv.reader(f)
+    
+#     for row in reader :
+#         print row
+    
+    
+#     #ref for https://www.tutorialspoint.com/python/python_for_loop.htm
+#     for index in range(header_Length) :
+#         
+#         tmp_Str = fin.read()
+#         
+#         print tmp_Str
+        
+        
+#     data_Csv = fin.readlines()
+    
+        
+    
+    # validate
+    if reader is None:
+#     if data_Csv is None:
+        
+        print "[%s:%d] read lines => None" % (libs.thisfile(), libs.linenum())
+        
+        return None
+    else:
+        
+        '''###################
+            skip header        
+        ###################'''
+        count = 0
+        
+        for row in reader : 
+            print(row)
+            
+#             ['1', '112.679', '112.706', '112.646', '112.672', '44.91202147258796', '48.05020
+#             210044262', '112.782302005599', '112.7065760027994', '112.6308499999999', '112.5
+#             551239972003', '112.4793979944007', '-0.007000000000005002', '0.0600000000000022
+#             7', '2017.12.29 23:00', '2017.12.30 06:00']
+
+            count += 1
+            
+            if count >= 2 : break
+        
+        print
+        print "[%s:%d] row =>" % (libs.thisfile(), libs.linenum())
+        
+        print
+        for row in reader : 
+            print(row)
+            print
+            break
+        
+        
+#         print
+#         print "[%s:%d] print row" % (libs.thisfile(), libs.linenum())
+#         
+#         for row in reader : 
+#             print(row)
+#             break
+#         
+#         print
+#         print "[%s:%d] print row" % (libs.thisfile(), libs.linenum())
+#         
+#         for row in reader : 
+#             print(row)
+#             break
+        
+        #ref len https://duckduckgo.com/?q=python+array+length&atb=v84-1__&ia=qa
+#         print "[%s:%d] lines => %d" % (libs.thisfile(), libs.linenum(), len(reader))
+        
+        #ref len of reader https://bytes.com/topic/python/answers/652839-csv-reader-length
+#         reader_Listed = list(reader)
+#         print "[%s:%d] lines => %d" % \
+#                     (libs.thisfile(), libs.linenum(), len(reader_Listed))   #=> lines => 722
+#                     (libs.thisfile(), libs.linenum(), len(list(reader)))
+#         print "[%s:%d] lines => %d" % (libs.thisfile(), libs.linenum(), len(data_Csv))
+        
+#         print (reader_Listed[0])    #=> ['Pair=USDJPY;Period=H1;Days=720;Shift=1;Bars=17280;Time=20171231_233725']
+#         print (reader_Listed[0][0]) #=> Pair=USDJPY;Period=H1;Days=720;Shift=1;Bars=17280;Time=20171231_233725
+#         print (list(reader))
+        
+#         row_1 = reader_Listed[0]
+#         
+#         for col in row_1 :
+#             
+#             print (col)
+#             
+#             print
+        
+#         '''###################
+#             first row        
+#         ###################'''
+#         print "[%s:%d] row '%d' => " % \
+#                     (libs.thisfile(), libs.linenum(), header_Length + id_Start)
+#         
+#         print (list(reader)[header_Length + id_Start])
+# #         print (reader[header_Length + id_Start])    #=> IndexError: list index out of range
+#     
+    '''###################
+        file : close
+    ###################'''
+    f.close()
+#     fin.close()
+    
+    print "[%s:%d] file => closed : %s" % \
+                (libs.thisfile(), libs.linenum(), fname_In)
+
+    #ref None https://stackoverflow.com/questions/3289601/null-object-in-python
+    return None
+
