d39b2510758bf3b04fd1917e57abd9653c87893d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2018-01-08 15:44:33 +0900

    FX / 22-1 s-62*82#3 / fx programming / 20180108_
    ------
    build array of BarData instances (first try)
    
            ==> done

--- free/fx/82_/1_/82_1.py
+++ free/fx/82_/1_/82_1.py
@@ -44,29 +44,85 @@ import random as rnd
 
 ###############################################
 
-def exec_prog():
+def exec_prog(): # upto : 20180108_122418
+     
+    '''######################################
+        get data : raw csv rows
+    ######################################'''
+    fname_In = "../data/49_11_file-io.USDJPY.Period-H1.Days-720.Bars-17280.20171231_233725.csv"    #=>
+ 
+    header_Length   = 3
+    skip_Header     = True
+#     skip_Header     = False
+ 
+    result = libfx.get_ChartData_CSV(\
+                    fname_In, header_Length, skip_Header)
+    
+    ### Validate
+    if result == None : #if result == None
+    
+        print "[%s:%d] get_ChartData_CSV => Returned 'None'" % \
+                    (libs.thisfile(), libs.linenum())
+        
+        return
+        
+    #/if result == None
+    
+    ### report
+    print
+    print "[%s:%d] CSV rows => %d" % (libs.thisfile(), libs.linenum(), len(result))
+    print
+    
+#     print "[%s:%d] row[%d] => %s" % (libs.thisfile(), libs.linenum(), 0, result[0])
+#     print
+    
+    '''######################################
+        Conv : CSV rows ---> array of BarData class instances        
+    ######################################'''
+    aryOf_BarDatas = libfx.conv_CSVRows_2_BarDatas(result)
+    
+    ### Validate
+    if aryOf_BarDatas == None : #if aryOf_BarDatas == None
+    
+        print "[%s:%d] aryOf_BarDatas => None" % (libs.thisfile(), libs.linenum())
+        print
+        
+        return
+    #/if aryOf_BarDatas == None
+    
+    
+    
     
     '''###################
-        get data        
+        Report        
     ###################'''
-    fname_In = "../data/49_11_file-io.USDJPY.Period-H1.Days-720.Bars-17280.20171231_233725.csv"    #=>
-
-    id_Start    = 195
-    id_End      = 202
-
-    header_Length = 3
-    skip_Header=True
-
-    result = libfx.get_ChartData_CSV_Between(\
-                    fname_In, id_Start, id_End, header_Length, skip_Header)
-#     result = libfx.get_ChartData_CSV_Between(fname_In, id_Start, id_End)
-    
-    print "[%s:%d] result => %s" % (libs.thisfile(), libs.linenum(), result)
-    
-    
     print "[%s:%d] exec_prog => done" % (libs.thisfile(), libs.linenum())
+ 
+#/def exec_prog()
 
-#def exec_prog()
+# def exec_prog(): # upto : 20180108_122418
+#     
+#     '''###################
+#         get data        
+#     ###################'''
+#     fname_In = "../data/49_11_file-io.USDJPY.Period-H1.Days-720.Bars-17280.20171231_233725.csv"    #=>
+# 
+#     id_Start    = 195
+#     id_End      = 202
+# 
+#     header_Length = 3
+#     skip_Header=True
+# 
+#     result = libfx.get_ChartData_CSV_Between(\
+#                     fname_In, id_Start, id_End, header_Length, skip_Header)
+# #     result = libfx.get_ChartData_CSV_Between(fname_In, id_Start, id_End)
+#     
+#     print "[%s:%d] result => %s" % (libs.thisfile(), libs.linenum(), result)
+#     
+#     
+#     print "[%s:%d] exec_prog => done" % (libs.thisfile(), libs.linenum())
+# 
+# #def exec_prog()
 
 '''
 <usage>
@@ -89,5 +145,10 @@ if __name__ == "__main__" :
     print
 #     msg = "[%s:%d] done" % (___FILE, ___FILE)
 #     msg = "[%s:%d] done" % (thisfile(), linenum())
-    msg = "[%s:%d] get_ChartData_Between(fname, id_Start, id_End) => done" % (libs.thisfile(), libs.linenum())
+    msg = "[%s:%d] get_ChartData_Between(fname, id_Start, id_End) => done" % \
+                (libs.thisfile(), libs.linenum())
+                
     print (msg)
+
+
+#####################################/EOF
--- free/fx/82_/libs/libfx.py
+++ free/fx/82_/libs/libfx.py
@@ -11,6 +11,8 @@ import os
 from time import gmtime, strftime, localtime, time
 from __builtin__ import str
 from sympy.physics.vector.printing import params
+from sympy.matrices.densetools import row
+from _ast import If
 # from curses.ascii import NUL
 
 '''###################
@@ -29,6 +31,7 @@ def test_func():
     
 
 # def get_ChartData_CSV_Between(fname_In, id_Start, id_End):
+
 def get_ChartData_CSV_Between \
 (fname_In, id_Start, id_End, header_Length, skip_Header=True):
     
@@ -166,4 +169,237 @@ def get_ChartData_CSV_Between \
 
     #ref None https://stackoverflow.com/questions/3289601/null-object-in-python
     return None
+
+'''###################
+    func : def get_ChartData_CSV
+
+    at : 2018/01/07 12:26:55
+
+    @return: [[csv row], [csv row], ...]
+            
+###################'''
+def get_ChartData_CSV \
+(fname_In, header_Length, skip_Header=True, delim = ';'):
+    
+    '''###################
+        vars
+    ###################'''
+    aryOf_CSV_Rows = []
+    
+    '''###################
+        file : open
+    ###################'''
+#     fname_In = "data/49_11_file-io.USDJPY.Period-H1.Days-720.Bars-17280.20171231_233725.csv"    #=> No such file or directory
+#     fname_In = "../data/49_11_file-io.USDJPY.Period-H1.Days-720.Bars-17280.20171231_233725.csv"    #=> 
+    
+    #ref csv open http://www.pythonforbeginners.com/systems-programming/using-the-csv-module-in-python/
+    f = open(fname_In, 'rb')
+    
+    print "[%s:%d] file => opened : %s" % (libs.thisfile(), libs.linenum(), fname_In)
+    
+    '''###################
+        file : read
+    ###################'''
+    '''###################
+        skip header
+    ###################'''
+    print "[%s:%d] skip headers => %d lines" % \
+                    (libs.thisfile(), libs.linenum(), header_Length)
+    
+    #ref delimiter https://docs.python.org/3.5/library/csv.html
+#     delim = ';'
+    reader = csv.reader(f, delimiter = delim)
+    
+    # validate
+    if reader is None:
+#     if data_Csv is None:
+        
+        print "[%s:%d] read lines => None" % (libs.thisfile(), libs.linenum())
+        
+        return None
+    
+    else:   # if reader is None:
+        
+        '''###################
+            skip header        
+        ###################'''
+        if skip_Header == True : #if skip_Header == True
+
+            count = 1
+            
+            for row in reader :
+                
+                count += 1
+                
+                if count >= header_Length : #if count >= header_Length
+#                 if count > header_Length : #if count >= header_Length
+                
+                    break
+                    
+                #/if count >= header_Length
+                
+            #/for row in reader :
+            
+            print "[%s:%d] header skipped => %d lines" % \
+                        (libs.thisfile(), libs.linenum(), header_Length)
+            
+        #/if skip_Header == True
+
+
+        
+        '''###################
+            read rows        
+        ###################'''
+        count = 0
+        
+        for row in reader : 
+#             print(row)
+            
+#             ['1', '112.679', '112.706', '112.646', '112.672', '44.91202147258796', '48.05020
+#             210044262', '112.782302005599', '112.7065760027994', '112.6308499999999', '112.5
+#             551239972003', '112.4793979944007', '-0.007000000000005002', '0.0600000000000022
+#             7', '2017.12.29 23:00', '2017.12.30 06:00']
+
+            count += 1
+            
+            
+#             if count >= 2 : break
+
+            aryOf_CSV_Rows.append(row)
+        
+        #/for row in reader :
+        
+#         #debug
+#         print
+#         print "[%s:%d] num of rows => %d" % (libs.thisfile(), libs.linenum(), count)
+#         print
+        
+#         print
+#         print "[%s:%d] row =>" % (libs.thisfile(), libs.linenum())
+#         
+#         print
+#         for row in reader : 
+#             print(row)
+#             print
+#             break
+        
+    #/if reader is None:
+    
+    '''###################
+        file : close
+    ###################'''
+    f.close()
+    
+    print "[%s:%d] file => closed : %s" % \
+                (libs.thisfile(), libs.linenum(), fname_In)
+
+    #ref None https://stackoverflow.com/questions/3289601/null-object-in-python
+#     return None
+    
+    return aryOf_CSV_Rows
+
+'''###################
+    conv_CSVRows_2_BarDatas(result)
+    
+    @param result: Array of CSV rows (Without header lines)
+        [['1', '112.679', '112.706', ...], [...], ...]
+    
+    @return: Array of BarData class instances
+        [barData, barData, ...]
+    
+###################'''
+def conv_CSVRows_2_BarDatas(result) :
+    
+#     print "[%s:%d] result[0] => %s" % (libs.thisfile(), libs.linenum(), result[0])
+#     print
+    
+    #debug
+    print "[%s:%d] len(result) => %d" % \
+                (libs.thisfile(), libs.linenum(), len(result))
+    print
+    
+    '''###################
+        Vars        
+    ###################'''
+    aryOf_BarDatas = []
+    
+    '''###################
+        Conversions        
+    ###################'''
+    for item in result :
+        
+        barData = BarData()
+    
+        # insert data
+            # ['1',    no,
+            # '112.679',    Open,
+            # '112.706',    High,
+            # '112.646',    Low,
+            # '112.672',    Close,
+            # '44.91202147258796',    RSI,
+            # '48.05020210044262',    MFI,
+            # '112.782302005599',    BB.2s,
+            # '112.7065760027994',    BB.1s,
+            # '112.6308499999999',    BB.main,
+            # '112.5551239972003',    BB.-1s,
+            # '112.4793979944007',    BB.-2s,
+            # '-0.007000000000005002',    Diff,
+            # '0.06000000000000227',    High/Low,
+            # '2017.12.29 23:00',    datetime,
+            # '2017.12.30 06:00']    
+            
+        barData.no            = int(item[0])
+        barData.price_Open    = float(item[1])
+        barData.price_High    = float(item[2])
+        barData.price_Low     = float(item[3])
+        barData.price_Close   = float(item[4])
+        
+        # append
+        aryOf_BarDatas.append(barData)
+        
+    #/for item in result :
+    
+    #debug
+    print "[%s:%d] len(aryOf_BarDatas) => %d" % \
+                (libs.thisfile(), libs.linenum(), len(aryOf_BarDatas))
+    print
+    
+    #debug
+    print "[%s:%d] aryOf_BarDatas[%d].id => %d" % \
+                (libs.thisfile(), libs.linenum(), 0, aryOf_BarDatas[0].no)
+    print "[%s:%d] aryOf_BarDatas[%d].price_Open => %.3f" % \
+                (libs.thisfile(), libs.linenum(), 0, aryOf_BarDatas[0].price_Open)
+    print "[%s:%d] aryOf_BarDatas[%d].id => %d" % \
+                (libs.thisfile(), libs.linenum(), 1, aryOf_BarDatas[1].no)
+    print "[%s:%d] aryOf_BarDatas[%d].id => %d" % \
+                (libs.thisfile(), libs.linenum(), 2, aryOf_BarDatas[2].no)
+    print
+    
+    #test
+#     barData_0 = BarData()
+#     
+#     # insert data
+#     barData_0.id            = result[0][0]
+#     barData_0.price_Open    = result[0][1]
+#     barData_0.price_High    = result[0][2]
+#     barData_0.price_Low     = result[0][3]
+#     barData_0.price_Close   = result[0][4]
+#     
+#     print "[%s:%d] barData_0 => %s" % (libs.thisfile(), libs.linenum(), barData_0)
+#     print
+    
+    return None
+
+#/def conv_CSVRows_2_BarDatas(result) :
+
+class BarData :
+    
+#     id = -1
+    no = -1
+    price_Open = -1.0
+    price_High = -1.0
+    price_Low = -1.0
+    price_Close = -1.0
+    
+#/class BarData :
 
