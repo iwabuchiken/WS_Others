892c70b0df522e419b2abb494361f34f33cfd19d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2018-01-28 09:16:48 +0900

    Prog / D-34 5#1 / Cake_IFM11 / use django / 20180128_
    steps=~1.5.2
    ---
    #INSTALLED_APPS
    
            ==> done

--- prog/D-34/5_/console2.py
+++ prog/D-34/5_/console2.py
@@ -52,13 +52,46 @@ python -m pip install django==1.10
         Installing collected packages: django
         Successfully installed django-1.10
 
+## https://overiq.com/django/1.10/installing-django/#testing-the-installation
+pushd C:\WORKS_2\WS\WS_Others\prog\D-34\5_\VIRTUAL
+python
+
+import django
+django.get_version()
+
+------------------------------------------------ 2018/01/28 08:58:26
+pushd C:\WORKS_2\WS\WS_Others\prog\D-34\5_\VIRTUAL
+django-admin startproject admin_CakeIFM_11
+cd admin_CakeIFM_11
+
+python manage.py runserver
+
+pushd C:\WORKS_2\WS\WS_Others\prog\D-34\5_\VIRTUAL\admin_CakeIFM_11
+python manage.py migrate > migrate.log
+
+    (env) C:\WORKS_2\WS\WS_Others\prog\D-34\5_\VIRTUAL>pushd C:\WORKS_2\WS\WS_Others\prog\D-34\5_\VIRTUAL\admin_CakeIFM_11
+    python manage.py migrate > migrate.log
 
 
+    (env) C:\WORKS_2\WS\WS_Others\prog\D-34\5_\VIRTUAL\admin_CakeIFM_11>Operations to perform:
+      Apply all migrations: admin, auth, contenttypes, sessions
+    Running migrations:
+      Rendering model states... DONE
+      Applying contenttypes.0001_initial... OK
+      Applying auth.0001_initial... OK
+      Applying admin.0001_initial... OK
+      Applying admin.0002_logentry_remove_auto_add... OK
+      Applying contenttypes.0002_remove_content_type_name... OK
+      Applying auth.0002_alter_permission_name_max_length... OK
+      Applying auth.0003_alter_user_email_max_length... OK
+      Applying auth.0004_alter_user_username_opts... OK
+      Applying auth.0005_alter_user_last_login_null... OK
+      Applying auth.0006_require_contenttypes_0002... OK
+      Applying auth.0007_alter_validators_add_error_messages... OK
+      Applying auth.0008_alter_user_username_max_length... OK
+      Applying sessions.0001_initial... OK
 
-
-
-
-
+python manage.py startapp im
 
 
 
