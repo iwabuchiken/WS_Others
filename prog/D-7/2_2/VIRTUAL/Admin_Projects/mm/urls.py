269f4cc4e97cf08ddceef3bb20ca1b1987bc1db0
iwabuchiken <iwabuchi.k.2010@gmail.com>
2018-01-30 18:20:04 +0900

    myself] Prog / D-7 2#2 / Projects Admin / mindmap files / 20180130_
    steps=~4.4
    ---
    edit : C:\WORKS_2\WS\WS_Others\prog\D-7\2_2\VIRTUAL\Admin_Projects\Admin_Projects\urls.py
    
            ==> done

--- prog/D-7/2_2/VIRTUAL/Admin_Projects/mm/urls.py
+++ prog/D-7/2_2/VIRTUAL/Admin_Projects/mm/urls.py
@@ -13,16 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import url
 
 from mm import views
 
 urlpatterns = [
     
 #     url(r'^$', views.index),
-    url(r'^mm/', include('mm.urls')),
-    
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='blog_index'),
     
 ]
