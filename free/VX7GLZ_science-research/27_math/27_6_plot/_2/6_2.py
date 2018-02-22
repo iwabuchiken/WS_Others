14b95e1d7d277c131c7597c0fd06aa9ea76a17f3
iwabuchiken <iwabuchi.k.2010@gmail.com>
2018-02-22 12:09:40 +0900

    myself] res free# VX7GLZ 27#6.2-3 / mathematics / plot / 20180222_
    ---
    def test_3()
        #) rotate ==> [cos, -sin][sin,cos]
    
    	==> done

--- free/VX7GLZ_science-research/27_math/27_6_plot/_2/6_2.py
+++ free/VX7GLZ_science-research/27_math/27_6_plot/_2/6_2.py
@@ -26,10 +26,10 @@ $1$2$3os.path.basename($4)$5
 
 
 import sys, os
-from cProfile import label
-from pip._vendor.requests.sessions import session
-import cmd
-from mailbox import fcntl
+# from cProfile import label
+# from pip._vendor.requests.sessions import session
+# import cmd
+# from mailbox import fcntl
 
 '''###################
     import : original files        
@@ -63,8 +63,190 @@ import xml.etree.ElementTree as ET, math as math, matplotlib.pyplot as plt \
         , numpy as np, matplotlib.patches as mpatches
 from shutil import copyfile
 from scipy.stats.stats import pearsonr
+from matplotlib.patches import Ellipse, Arc
+from math import pi
 
 ###############################################
+def _test_3__Generate_PNGFiles(dpath_Full, session_Label):
+    
+    '''###################
+        gen        
+    ###################'''
+        # dirs
+    if not os.path.isdir(dpath_Full) : os.makedirs(dpath_Full)
+
+    '''######################################
+        plot        
+    ######################################'''
+#     '''###################
+#         settings        
+#     ###################'''
+#     plt.xlim(-2 * np.pi,2 * np.pi)
+#     plt.ylim(-2,2)
+#     plt.grid(b=None, which='major', axis='both')
+    
+    tlabel = libs.get_TimeLabel_Now()
+    
+    x = np.linspace(-2 * np.pi, 2 * np.pi, 500)
+    
+#     fig = plt.figure()
+#     ax  = fig.add_subplot(111)
+
+    '''###################
+        constants        
+    ###################'''
+    lim_X_Start = -5
+    lim_X_End = 5
+    lim_Y_Start = -5
+    lim_Y_End = 5
+    
+    cnt = 0
+    
+    x = 1; y = 0
+    
+    for index in np.arange(0, np.pi * 2, np.pi / 6):
+
+        '''###################
+            settings        
+        ###################'''
+#         plt.xlim(-2, 2)
+#         plt.ylim(-2, 2)
+        
+        plt.grid(b=None, which='major', axis='both')
+
+#         plt.axes().set_aspect('equal', 'datalim')
+
+        fig = plt.figure()
+        
+        #ref https://stackoverflow.com/questions/45771474/matplotlib-make-center-circle-transparent
+        fig.set_facecolor("#fff9c9")
+        
+        ax  = fig.add_subplot(111)
+
+        ax.grid(b=None, which='major', axis='both')
+        
+        #ref https://stackoverflow.com/questions/7965743/how-can-i-set-the-aspect-ratio-in-matplotlib
+        ax.set_aspect('equal')
+
+        #ref https://stackoverflow.com/questions/15858192/how-to-set-xlim-and-ylim-for-a-subplot-in-matplotlib answered Apr 7 '13 at 2:33
+        
+        ax.set_xlim(lim_X_Start, lim_X_End)
+        ax.set_ylim(lim_Y_Start, lim_Y_End)
+#         ax.set_xlim(-5,5)
+#         ax.set_ylim(-5,5)
+#         ax.set_xlim(-2, 2)
+#         ax.set_ylim(-2, 2)
+
+        '''###################
+            label : y
+        ###################'''
+        #ref https://stackoverflow.com/questions/10729737/how-can-i-set-the-y-axis-in-radians-in-a-python-plot
+        tickVal_Y = 1
+        tickVal_X = 1
+        
+        # y ax
+        y_tick = np.arange(lim_Y_Start, lim_Y_End + tickVal_Y, tickVal_Y)
+        ax.set_yticks(y_tick)
+        ax.set_yticklabels(y_tick, fontsize = 10)
+
+        # x ax
+        x_tick = np.arange(lim_X_Start, lim_X_End + tickVal_X, tickVal_X)
+        ax.set_xticks(x_tick)
+        ax.set_xticklabels(x_tick, fontsize = 10)
+
+        '''###################
+            circle        
+        ###################'''
+#         #ref https://stackoverflow.com/questions/4143502/how-to-do-a-scatter-plot-with-empty-circles-in-python
+#         ax.scatter(0, 0, s = 600, facecolors='none', edgecolors='r')
+#         plt.scatter(x, y, s=80, facecolors='none', edgecolors='r')
+        
+        
+        #ref https://stackoverflow.com/questions/45771474/matplotlib-make-center-circle-transparent
+        circle1 = plt.Circle((0, 0), 1.0, color='r', fill= False, linewidth = 1, alpha = 0.5)
+#         circle1 = plt.Circle((0, 0), 1.0, color='r', fill= False, linewidth = 1)
+#         circle1 = plt.Circle((0, 0), 1.0, color='r', fill= False, linewidth = 10)
+        #ref https://stackoverflow.com/questions/9215658/plot-a-circle-with-pyplot
+#         circle1 = plt.Circle((0, 0), 1.0, color='r')
+         
+        ax.add_artist(circle1)
+
+        #ref https://stackoverflow.com/questions/20634170/drawing-obtuse-circular-arc-with-arrowhead-in-matplotlib answered Dec 17 '13 at 14:42
+        #Arc(xy, width, height[, angle, theta1, theta2])
+#         ellipse = Arc([0,2.5],10,10,0,0,pi,color='green', linewidth='10')
+#         ellipse = Arc([0,2.5],5,5,0,0,pi,color='green', linewidth='10')
+#         ellipse = Arc([2.5,2.5],5,5,0,0,pi,color='green', linewidth='10')
+#         ellipse = Arc([2.5,2.5],1,1,0,0,pi,color='green', linewidth='0.5')
+        
+#         ax.add_patch(ellipse)
+
+        '''###################
+            labels        
+        ###################'''
+
+        '''###################
+            y values
+        ###################'''
+        rot = [
+            
+                [np.cos(index), - np.sin(index)],
+                [np.sin(index),   np.cos(index)],
+            
+            ]
+        
+        x_ = rot[0][0] * x  - rot[0][1] * y
+        y_ = rot[1][0] *  x + rot[1][1] * y
+        
+        '''###################
+            plot        
+        ###################'''
+        ax.scatter([x_], [y_], s = 40, color = 'b')
+#         ax.scatter([x_], [y_], s = 20)
+#         plt.scatter([x_], [y_], s = 20)
+        
+        '''###################
+            ops        
+        ###################'''
+        fpath_Images_Out = "%s\\%s.%s.(%02d).png" \
+                    % (dpath_Full, session_Label, tlabel, cnt)
+        
+        plt_Title = "rot : %.03f pi\n%s" % (index / np.pi, os.path.basename(fpath_Images_Out))
+        plt.title(plt_Title)
+        
+        plt.plot(x, y)
+        
+        plt.savefig(fpath_Images_Out)
+#         #debug
+#         plt.show()
+        
+        cnt += 1
+
+#         #debug
+#         print()
+#         print("[%s:%d] breaking the loop..." % \
+#             (os.path.basename(libs.thisfile()), libs.linenum()
+#               
+#             ), file=sys.stderr)
+#         break
+
+        # clear
+#         plt.clf()
+        
+        '''###################
+            clear        
+        ###################'''
+        plt.close(fig)
+        
+    #/for index in np.arange(0, np.pi * 2, np.pi / 12):
+
+    print()
+    print("[%s:%d] image files saved => %d" % \
+        (os.path.basename(libs.thisfile()), libs.linenum()
+        , cnt
+        ), file=sys.stderr)
+    
+#/def _test_2__Generate_PNGFiles(dpath_Full):
+    
 def _test_2__Generate_PNGFiles(dpath_Full, session_Label):
     
     '''###################
@@ -163,6 +345,57 @@ def _test_2__Generate_PNGFiles(dpath_Full, session_Label):
     
 #/def _test_2__Generate_PNGFiles(dpath_Full):
     
+def test_3():
+    
+    '''###################
+        get : paths        
+    ###################'''
+    tlabel = libs.get_TimeLabel_Now()
+
+    PROJECT_ROOT = cons_27_6_2.FPath.PROJECT_ROOT.value
+    
+    dname_Folder_Data   = "data.27_6_2"
+    dname_Images       = "images"
+    dname_Images_PNG   = "images_%s" % tlabel
+    session_Label      = "6_2-2.test-3"
+    
+    fps_FFMpeg          = 2
+    
+    dpath_Full, fpath_Glob, fpath_In_FFMpeg, fpath_Out_FFMpeg = \
+                libs_VX7GLZ.get_FFMpeg_Paths \
+                (PROJECT_ROOT, 
+                 dname_Folder_Data, 
+                 dname_Images, 
+                 dname_Images_PNG, 
+                 session_Label)
+    
+#     #ref https://stackoverflow.com/questions/394809/does-python-have-a-ternary-conditional-operator
+#     msg = "exists" if (os.path.isdir(os.path.dirname(dpath_Full))) else "NOT"
+# #     msg = (os.path.isdir(os.path.dirname(dpath_Full)) == True) ? "exists" : "NOT"
+#     
+#     print()
+#     print("[%s:%d] dpath_Full => %s (%s)" % \
+#         (os.path.basename(libs.thisfile()), libs.linenum()
+#         , os.path.dirname(dpath_Full), msg
+# #         , dpath_Full, msg
+#         ), file=sys.stderr)
+    
+    '''###################
+        gen : png files        
+    ###################'''
+    result = _test_3__Generate_PNGFiles(dpath_Full, session_Label)
+    
+    '''###################
+        video        
+    ###################'''
+    result = libs_VX7GLZ.build_Video_From_PNGFiles__V3(
+                    fpath_Glob, fpath_In_FFMpeg, fpath_Out_FFMpeg, fps_FFMpeg)
+#     result = libs_VX7GLZ.build_Video_From_PNGFiles__V2(
+#                     fpath_Glob, fpath_In_FFMpeg, fpath_Out_FFMpeg)
+    
+    
+#/def test_2():
+
 def test_2():
     
     '''###################
@@ -315,7 +548,8 @@ def exec_prog(): # from :
     '''###################
         ops        
     ###################'''
-    test_2()
+    test_3()
+#     test_2()
 #     test_1()
     
     '''###################
--- free/VX7GLZ_science-research/libs_VX7GLZ/libs_VX7GLZ.py
+++ free/VX7GLZ_science-research/libs_VX7GLZ/libs_VX7GLZ.py
@@ -367,6 +367,143 @@ def build_Video_From_PNGFiles__V2 \
     return 0
 
 '''###################
+    @param fps_FFMpeg: num of frames per second
+    
+    @return: 
+        -1    glob ==> returned less than 1
+        0     video ==> generated
+###################'''
+def build_Video_From_PNGFiles__V3 \
+(fpath_Glob, fpath_In_PNGFiles, fpath_Out_FFMpeg, fps_FFMpeg = 2):
+    
+    '''###################
+        file path        
+    ###################'''
+
+    '''###################
+        png files        
+    ###################'''
+    #ref glob https://stackoverflow.com/questions/14798220/how-can-i-search-sub-folders-using-glob-glob-module-in-python answered Feb 10 '13 at 13:31    
+    lo_Files = glob.glob(fpath_Glob)
+    
+    ### validate
+    if len(lo_Files) < 1 : #if len(lo_Files) < 1
+
+        print()
+        print("[%s:%d] len(lo_Files) => less than 1" % \
+            (os.path.basename(libs.thisfile()), libs.linenum()
+            , len(lo_Files), fpath_Glob
+            ), file=sys.stderr)
+        
+        return -1
+    
+    else :
+        
+        print()
+        print("[%s:%d] len(lo_Files) => %d" % \
+            (os.path.basename(libs.thisfile()), libs.linenum()
+            , len(lo_Files)
+            ), file=sys.stderr)
+    #/if len(lo_Files) < 1
+
+    '''###################
+        sort : list        
+    ###################'''
+    #ref https://www.programiz.com/python-programming/methods/list/sort
+    lo_Files.sort()
+    
+    '''###################
+        copy        
+    ###################'''
+    cnt = 0
+    
+    lo_CopiedFiles = []
+    
+    for item in lo_Files:
+     
+        dpath = os.path.dirname(item)
+         
+        fpath_Dst = "%s\\image.%03d.png" % (dpath, cnt)
+         
+         
+         
+        copyfile(item, fpath_Dst)
+        
+        # append new file path
+        lo_CopiedFiles.append(fpath_Dst)
+         
+#         print()
+#         print("[%s:%d] file copied : %s => %s" % \
+#                 (os.path.basename(libs.thisfile()), libs.linenum()
+#                 , item, fpath_Dst
+#                 ), file=sys.stderr)
+        
+        # increment
+        cnt += 1
+        
+    #/for item in lo_Files:
+
+    '''###################
+        video : generate        
+    ###################'''
+    cmd = "ffmpeg -f image2 -r %d -i %s -vcodec mpeg4 -y %s" \
+            % (fps_FFMpeg, fpath_In_PNGFiles, fpath_Out_FFMpeg)
+#     cmd = "ffmpeg -f image2 -r 2 -i " \
+#             + "%s -vcodec mpeg4 -y %s" % (fpath_In_PNGFiles, fpath_Out_FFMpeg)
+
+    # execute
+    os.system(cmd)
+    
+    print()
+    print("[%s:%d] executing command => %s" % \
+        (os.path.basename(libs.thisfile()), libs.linenum()
+        , cmd
+        ), file=sys.stderr)
+
+    '''###################
+        delete : copied files        
+    ###################'''
+    while (not os.path.isfile(fpath_Out_FFMpeg)) :
+        
+        pass
+    
+    #/while (not os.path.isfile(fpath_Out)) :
+
+#     print()
+#     print("[%s:%d] copied files ==> deleting..." % \
+#             (os.path.basename(libs.thisfile()), libs.linenum()
+#             
+#             ), file=sys.stderr)
+    
+    # remove files
+    for item in lo_CopiedFiles:
+    
+        if os.path.isfile(item) : #if os.path.isfile(item)
+    
+            res = os.remove(item)
+            
+#             print()
+#             print("[%s:%d] remove file : result => %s (%s)" % \
+#                     (os.path.basename(libs.thisfile()), libs.linenum()
+#                     , res, item
+#                     ), file=sys.stderr)
+            
+        #/if os.path.isfile(item)
+    
+    print()
+    print("[%s:%d] copied files ==> deleted" % \
+            (os.path.basename(libs.thisfile()), libs.linenum()
+            
+            ), file=sys.stderr)    
+        
+    #/for item in lo_CopiedFiles:
+
+    '''###################
+        return        
+    ###################'''
+    return 0
+
+'''###################
     <Ops>
     1. Scatter each point in z
     2. Scatter figure ---> save as png file
